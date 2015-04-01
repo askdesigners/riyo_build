bc20ccae326ab30f366490d19dd5764f697ba248
Ryan Cole <ryan@askdesigners.eu>
2015-04-01 17:38:03 +0200

    fixing up the theme, when API error struck. Now stuck.

--- public/storage/themes/riyo/js/main.js
+++ public/storage/themes/riyo/js/main.js
@@ -1,38 +1,28 @@
 /* headnav scroll size */
 $(function(){
     $('#headnav').data('size','big');
-});
 
-$(window).scroll(function(){
-    var $nav = $('#headnav');
-    if ($('body').scrollTop() > 0) {
-        if ($nav.data('size') == 'big') {
-            $nav.data('size','small').stop().animate({
-                height:'45px',
-                opacity: 0.8
-            }, 600);
-            $("#headnav img").animate({
-                height:'35px'
-            }, 600);
+    $(window).scroll(function(){
+        var $nav = $('#headnav');
+        if ($('body').scrollTop() < 40) {
+
+            if($nav.hasClass('stuck')){
+                console.log('removing class');
+                $nav.removeClass('stuck');
+            }
+
+        } else {
+
+            if(!$nav.hasClass('stuck')){
+                console.log('adding class');
+                $nav.addClass('stuck');
+            }
         }
-    } else {
-        if ($nav.data('size') == 'small') {
-            $nav.data('size','big').stop().animate({
-                height:'90px',
-                opacity: 1
-            }, 600);
-            $("#headnav img").animate({
-                height:'80px'
-            }, 600);
-        }
-    }
-});
+    });
 
-
-/* Smooth scroll on nav click (needs some tweaking possibly, quickly written)*/
-jQuery(document).ready(function($) {
     $("#headnav a").click(function(event){
         event.preventDefault();
         $('html,body').animate({scrollTop:$(this.hash).offset().top - 90}, 1000);
     });
+
 });
